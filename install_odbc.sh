4e1727938910227a63d4ba4889b9f10ee28d01fa
John White <john@whites.site>
2025-04-06 15:04:39 -0400

    Clean up project: Remove unnecessary files and clean up .gitignore

--- install_odbc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
-curl https://packages.microsoft.com/config/debian/11/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
-ACCEPT_EULA=Y apt-get install -y mssql-tools18
-apt-get install -y unixodbc-dev 
